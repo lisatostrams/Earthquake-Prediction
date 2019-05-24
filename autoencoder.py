308fd8d46bf6f8610e41d1d7717a5491f0206b5d
Lisa Tostrams <ltostrams@machine2learn.nl>
2019-05-24 12:07:00 +0200

    autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -10,13 +10,14 @@ from keras.layers import Input, LSTM, RepeatVector
 from keras.models import Model
 import pandas as pd
 import numpy as np
-timesteps = 150000
+timesteps = 15000
 input_dim = 1
 
-latent_dim = 3
+latent_dim = 3000
 
 inputs = Input(shape=(timesteps, input_dim))
 encoded = LSTM(latent_dim)(inputs)
+
 
 decoded = RepeatVector(timesteps)(encoded)
 decoded = LSTM(input_dim, return_sequences=True)(decoded)
@@ -31,7 +32,7 @@ sequence_autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 reader = pd.read_csv("Data/train.csv",
                     dtype={'acoustic_data': np.int16, 'time_to_failure': np.float32},
-                    chunksize=150000)
+                    chunksize=timesteps)
 
 cols = ['latent_dim_{}'.format(i) for i in range(latent_dim)]
 Train = pd.DataFrame(index=range(4195),columns=cols)
@@ -39,9 +40,12 @@ i=0
 for df in reader:
     print(i)
     x_train = df['acoustic_data'].values.reshape(1,timesteps,input_dim)
-    sequence_autoencoder.fit(x_train, x_train, epochs=10, batch_size=25, shuffle=True, validation_data=(x_train, x_train))
+    sequence_autoencoder.fit(x_train, x_train, epochs=3, batch_size=int(timesteps/100), shuffle=True, validation_data=(x_train, x_train))
+    #sequence_autoencoder.save('model_trained_chunk_{}.h5'.format(i))
+    i=i+1
+    if(i==1):
+        break
+    
     #x_test = x_test.astype('float32') / 255.
     #x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
     #x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
-
-    
